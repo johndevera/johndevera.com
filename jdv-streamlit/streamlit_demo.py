7fa60ee84d4341ca9a879ea1962bb196e6b1f35f
John de Vera <john.devera@gmail.com>
2024-02-19 19:49:09 -0800

    feat(streamlit): add hello world example

--- jdv-streamlit/streamlit_demo.py
+++ jdv-streamlit/streamlit_demo.py
@@ -5,14 +5,19 @@ def main():
     st.title("CSV Viewer")
     
     # Upload CSV file
-    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+    # uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
     
-    if uploaded_file is not None:
-        # Read the CSV file
-        df = pd.read_csv(uploaded_file)
+    my_dataframe = pd.DataFrame({
+        'first column': [1, 2, 3, 4],
+        'second column': [10, 20, 30, 40]
+    })
+    st.dataframe(my_dataframe)
+    # if uploaded_file is not None:
+    #     # Read the CSV file
+    #     df = pd.read_csv(uploaded_file)
         
-        # Display the DataFrame
-        st.write(df)
+    #     # Display the DataFrame
+    #     st.write(df)
 
 if __name__ == "__main__":
     main()
